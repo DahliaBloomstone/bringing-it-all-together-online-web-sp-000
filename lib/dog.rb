e26583e3e4057360d9b0dc7770ba4ffbc7d24603
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-17 19:25:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,10 @@
+require_relative "../config/environment.rb"
+
 class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-def initialize(name:, breed:, id = nil)
+def initialize(name, breed, id = nil)
   @name = name
   @breed = breed
   @id = nil
