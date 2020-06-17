dbbb6a94bc1d001539640d414fdf4d31813e1b10
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-17 19:35:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -42,7 +42,8 @@ class Dog
   def self.find_by_id(id)
     sql = <<-SQL
       SELECT *
-      FROM dogs WHERE id = ?
+      FROM dogs
+      WHERE id = ?
     SQL
     DB[:conn].execute(sql, id).map do |row|
       self.new_from_db(row)
