da351e571a54330c853eb7d6d0b6cfc92ae47dc0
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-17 19:29:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,8 +8,15 @@ def initialize(id = nil, name, breed)
 @name = name
 @breed = breed
 end
-
-
+def self.create_table
+   sql =  <<-SQL
+     CREATE TABLE IF NOT EXISTS students (
+       id INTEGER PRIMARY KEY,
+       name TEXT,
+       grade TEXT
+       )
+   SQL
+   DB[:conn].execute(sql)
 
 
 
